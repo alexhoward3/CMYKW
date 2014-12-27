4024763ddc93a02d17008d30eccfc018cdbcbb40
Alex Howard <ahoward1024@gmail.com>
2014-12-27 14:48:54 -0700

    Background box - Background frame box now rotates 90 degrees left and right at each press of the corresponding arrow key. - Added clock functionality to the debug message string in the lower left corner.

---
+++ core/src/com/esw/cmyk/Box.java
@@ -0,0 +1,76 @@
+package com.esw.cmyk;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Box extends Sprite {
+	
+	private float rotation;
+	private boolean isRotating;
+	public boolean isRotatingRight;
+	public boolean isRotatingLeft;
+	
+	public Box() {
+		super();
+		rotation = super.getRotation();
+	}
+	
+	public Box(Texture texture) {
+		super(texture);
+		rotation = super.getRotation();
+	}
+	
+	@Override
+	public void rotate(float degrees) {
+		isRotating = true;
+		super.rotate(degrees);
+		rotation += degrees;
+		if(rotation >= 90 || rotation <= -90) {
+			resetRotation();
+			isRotating = false;
+		}
+	}
+	
+	public void rotateLeft(float degrees) {
+		isRotatingLeft = true;
+		this.rotate(degrees);
+		rotation += degrees;
+		if(rotation >= 90 || rotation <= -90) {
+			resetRotation(); 
+			isRotatingLeft = false;
+		}
+	}
+	
+	public void rotateRight(float degrees) {
+		isRotatingRight = true;
+		this.rotate(degrees);
+		rotation += degrees;
+		if(rotation >= 90 || rotation <= -90) {
+			resetRotation();
+			isRotatingRight = false;
+		}
+	}
+	
+	@Override
+	public float getRotation() {
+		return rotation;
+	}
+	
+	public void resetRotation() {
+		rotation = 0;
+	}
+	
+	public boolean isRotating() {
+		return isRotating;
+	}
+}
+
+
+
+
+
+
+
+
+
+
--- core/src/com/esw/cmyk/CMYK.java
+++ core/src/com/esw/cmyk/CMYK.java
@@ -1,44 +1,41 @@
 package com.esw.cmyk;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Rectangle;
 import com.esw.Meta;
 
-public class CMYK extends ApplicationAdapter {
+public class CMYK extends ApplicationAdapter implements ApplicationListener, InputProcessor {
 	public static int SCREEN_WIDTH = 1600; //TODO (Alex) Fix these constants!
 	public static int SCREEN_HEIGHT = 900;
-	
+
 	private SpriteBatch batch;
 	private Texture texture;
-	private Sprite box;
-	private Camera camera;
+	private Box box;
 	private BitmapFont debugMessage;
 	private String message = "Debug: ";
-	
+	private float deltaTime;
+	private float debugClock;
+
 	@Override
 	public void create () {
+		Meta.println("CMYKW");
 		batch = new SpriteBatch();
 		texture = new Texture(Gdx.files.internal("images/testbox.png")); //Path is /android/assets/...
 		debugMessage = new BitmapFont();
 		debugMessage.setColor(Color.GREEN);
+		box = new Box(texture);
 		
-		box = new Sprite(texture);
 		box.setCenter(SCREEN_WIDTH/2,  SCREEN_HEIGHT/2);
 	}
-	
+
 	@Override
 	public void dispose() {
 		batch.dispose();
@@ -49,35 +46,113 @@ public class CMYK extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1); //Set background to BLACK
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		deltaTime = Gdx.graphics.getDeltaTime();
+		debugClock += deltaTime;
 		
-		if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+		if(!box.isRotatingRight && Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
 			message = "Debug: left key pressed";
-			box.rotate90(false);
-		} else if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+			Meta.println("Rotating Left");
+			box.isRotatingRight = true;
+			debugClock = 0;
+		} else if(!box.isRotatingLeft && Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
 			message = "Debug: right key pressed";
-			box.rotate90(true);
+			Meta.println("Rotating Right");
+			box.isRotatingLeft = true;
+			debugClock = 0;
 		}
 		
-		//BEGIN DRAWING
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
+		
+		if(box.isRotatingLeft) {
+			box.rotateLeft(-10f); //This number must be -10f!
+		} else if(box.isRotatingRight) {
+			box.rotateRight(10f); //This number must be 10f!
+		}
+		
+		if(debugClock >= 2) { //About 2 seconds
+			message = "Debug:"; //Resets the debug message
+			debugClock = 0; //Resets the debug clock
+		}
+		
 		batch.begin();
-		box.draw(batch); //Draw the sprite
+		box.draw(batch);
 		debugMessage.draw(batch, message, 10, 40);
 		batch.end();
 		//END DRAWING
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
-		
+
 	}
-	
+
 	@Override
 	public void pause() {
-		
+
 	}
-	
+
 	@Override
 	public void resume() {
-		
+
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
+
+
+
+
+
+
+
+
+
+
